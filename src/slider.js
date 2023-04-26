a855232172f7a736489f59c680d24e7a738bd49e
A1ien1385 <damian.zawadzki@tlen.pl>
2023-04-26 23:11:56 +0200

    second commit

---
+++ src/slider.js
@@ -0,0 +1,35 @@
+import {Swiper, SwiperSlide} from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y, EffectCube } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import './App.css';
+
+export const Slider = ({slides}) => {
+    return (
+    <Swiper
+      effect={"cube"}
+      cubeEffect={{
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      }}
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCube]}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {slides.map((slide) => (
+         <SwiperSlide className='slide' key={slide.image}>
+                   <img src={slide.image} alt={slide.title}/>
+         </SwiperSlide> 
+      ))}
+      
+    </Swiper>)
+}
